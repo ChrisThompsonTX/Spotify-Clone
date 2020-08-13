17a15303f8e3a0e30e6d96a6b06cdaac28c33069
Chris Thompson <chris.tx86@gmail.com>
2020-08-12 18:36:55 -0700

    complete header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './header.css';
 import SearchIcon from '@material-ui/icons/Search';
-import { Avatar } from '@material-ui/core'
+import { Avatar } from '@material-ui/core';
+import { useDataLayerValue } from '../DataLayer';
 
 function Header({ spotify }) {
+    const [{ user }, dispatch ] = useDataLayerValue();
+
     return (
         <div className="header">
             <div className="header__left">
@@ -14,8 +17,8 @@ function Header({ spotify }) {
                 />
             </div>
             <div className="header__right">
-                <Avatar src="" alt="" />
-                <h4>Chris Thompson</h4>
+                <Avatar src={user?.images[0]?.url} alt={user?.display_name} />
+                <h4>{user?.display_name}</h4>
             </div>
         </div>
     )
