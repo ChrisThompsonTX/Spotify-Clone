21221ff9a2d0a6a31df1f732329b16668bcbe15f
Chris Thompson <chris.tx86@gmail.com>
2020-08-12 18:44:59 -0700

    style banner

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,6 +6,18 @@ function Body({ spotify }) {
     return (
         <div className="body">
             <Header spotify={spotify} />
+
+            <div className="body__info">
+                <img 
+                    src="" 
+                    alt="" 
+                />
+                <div className="body__infoText">
+                    <strong>PLAYLIST</strong>
+                    <h2>Discover Weekly</h2>
+                    <p>playlist....</p>
+                </div>
+            </div>
         </div>
     )
 }
